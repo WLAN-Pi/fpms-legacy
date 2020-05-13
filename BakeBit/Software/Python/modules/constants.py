acc16dfa6c64d6cad0170daa0ce342408b797174
Nigel Bowden <wifinigel@gmail.com>
2020-05-13 10:18:51 +0100

    Fixed key ap cfg file location

--- BakeBit/Software/Python/modules/constants.py
+++ BakeBit/Software/Python/modules/constants.py
@@ -5,6 +5,8 @@ constants.py - shared constant vars
 """
 
 from PIL import ImageFont
+import os
+import pathlib
 
 __version__ = "2.00 (alpha-8)"
 __author__ = "wifinigel@gmail.com"
@@ -14,6 +16,12 @@ PAGE_WIDTH = 128             # Pixel size of screen width
 PAGE_HEIGHT = 64             # Pixel size of screen height
 NAV_BAR_TOP = 55             # Top pixel number of nav bar
 MENU_VERSION =  __version__  # fpms version
+
+# figure out the script path
+SCRIPT_PATH = str(pathlib.Path(__file__).parent.parent.absolute())
+
+# change in to script path dir
+os.chdir(SCRIPT_PATH)
 
 # Define display fonts
 SMART_FONT = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 10)
@@ -26,6 +34,13 @@ FONTB14 =  ImageFont.truetype('DejaVuSansMono-Bold.ttf', 14)
 #######################################
 # File name constants
 #######################################
+
+# Linux programs
+IFCONFIG_FILE = '/sbin/ifconfig'
+IW_FILE = '/usr/sbin/iw'
+UFW_FILE = '/usr/sbin/ufw'
+ETHTOOL_FILE = '/sbin/ethtool'
+
 # Mode changer scripts
 WCONSOLE_MODE_FILE = '/etc/wconsole/wconsole.on'
 HOTSPOT_MODE_FILE = '/etc/wlanpihotspot/hotspot.on'
@@ -35,24 +50,20 @@ WCONSOLE_SWITCHER_FILE ='/etc/wconsole/wconsole_switcher'
 HOTSPOT_SWITCHER_FILE = '/etc/wlanpihotspot/hotspot_switcher'
 WIPERF_SWITCHER_FILE = '/home/wlanpi/wiperf/wiperf_switcher'
 
+#### Paths below here are relative to script dir or /tmp fixed paths ###
+
 # helper scripts to launch misc processes
-KISMET_CTL_FILE = '/home/wlanpi/fpms/BakeBit/Software/Python/scripts/kismet_ctl'
-BETTERCAP_CTL_FILE = '/home/wlanpi/fpms/BakeBit/Software/Python/scripts/bettercap_ctl'
-PROFILER_CTL_FILE = '/home/wlanpi/fpms/BakeBit/Software/Python/scripts/profiler_ctl'
+KISMET_CTL_FILE = SCRIPT_PATH + '/scripts/kismet_ctl'
+BETTERCAP_CTL_FILE = SCRIPT_PATH + '/scripts/bettercap_ctl'
+PROFILER_CTL_FILE = SCRIPT_PATH + '/scripts/profiler_ctl'
 
 # cdp and lldp networkinfo data file names
 LLDPNEIGH_FILE = '/tmp/lldpneigh.txt'
 CDPNEIGH_FILE = '/tmp/cdpneigh.txt'
-IPCONFIG_FILE = '/home/wlanpi/fpms/BakeBit/Software/Python/scripts/networkinfo/ipconfig.sh 2>/dev/null'
-REACHABILITY_FILE = '/home/wlanpi/fpms/BakeBit/Software/Python/scripts/networkinfo/reachability.sh'
-PUBLICIP_CMD = '/home/wlanpi/fpms/BakeBit/Software/Python/scripts/networkinfo/publicip.sh'
-
-# Linux programs
-IFCONFIG_FILE = '/sbin/ifconfig'
-IW_FILE = '/usr/sbin/iw'
-UFW_FILE = '/usr/sbin/ufw'
-ETHTOOL_FILE = '/sbin/ethtool'
+IPCONFIG_FILE = SCRIPT_PATH + '/scripts/networkinfo/ipconfig.sh 2>/dev/null'
+REACHABILITY_FILE = SCRIPT_PATH + '/scripts/networkinfo/reachability.sh'
+PUBLICIP_CMD = SCRIPT_PATH + 'scripts/networkinfo/publicip.sh'
 
 # key map file
-BUTTONS_FILE = "/home/wlanpi/fpms/buttons.txt"
+BUTTONS_FILE = SCRIPT_PATH + '/buttons.txt'
 
