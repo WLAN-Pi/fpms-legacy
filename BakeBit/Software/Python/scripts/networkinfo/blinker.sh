9236b6e5f068c67f7b58583d58bc4962dd43def6
Jiri Brejcha <jirka@jiribrejcha.net>
2020-07-31 11:43:26 +0000

    Enhanced version of blinker.sh script

--- BakeBit/Software/Python/scripts/networkinfo/blinker.sh
+++ BakeBit/Software/Python/scripts/networkinfo/blinker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#Deffault number of down & up cycles if not specified explicitly otherwise
+#Default number of down & up cycles if not specified explicitly otherwise
 COUNT=5
-
 #Default interface name if not specified explicitly otherwise
 INTERFACE="eth0"
 
@@ -11,59 +10,60 @@ usage(){
 }
 
 blink_n_times(){
+  echo "Interface: $INTERFACE"
   COUNT=$1
   for (( c=1; c<="$COUNT"; c++ ))
   do
-    echo "Down"
+    echo -e "\e[91mDown"
     sudo ifconfig "$INTERFACE" down
     sleep 3
-    echo "Up"
+    echo -e "\e[92mUp"
     sudo ifconfig "$INTERFACE" up
     sleep 7
   done
 }
 
 blink_n_seconds(){
+  echo "Interface: $INTERFACE"
   timeout $1 bash <<EOF
   echo "Timeout is $1"
   while true
   do
-    echo "Down"
+    echo -e "\e[91mDown"
     sudo ifconfig "$INTERFACE" down
     sleep 3
-    echo "Up"
+    echo -e "\e[92mUp"
     sudo ifconfig "$INTERFACE" up
     sleep 7
   done
 EOF
 }
 
-
-#Was any interface name passed as an argument
-if [[ "$*" == *'-i '* ]]
-then
-    INTERFACE=$(echo $@ | grep -o '\-i .*' | cut -d " " -f2)
-    echo "Got interface $INTERFACE"
-
+#Was at least 1 complete argument (2 strings separate by space) provided
+if [ "$#" -eq 1 ]; then
+  usage
+  exit
 fi
 
+#Was any interface name passed as an argument
+if [[ "$*" == *'-i '* ]]; then
+    INTERFACE=$(echo $@ | grep -o '\-i .*' | cut -d " " -f2)
+fi
 
 while [ "$1" != "" ]; do
     case $1 in
         -c | --count )          shift
-				echo "Got -c and it was $1"
                                 blink_n_times $1
                                 exit
                                 ;;
         -t | --timeout )        shift
-                                echo "Got -t and it was $1"
                                 blink_n_seconds $1
                                 exit
                                 ;;
         -h | --help )           usage
                                 exit
                                 ;;
-        * )                     if [ "$#" -ge 1 ]; then usage; exit; fi
+        * )
     esac
     shift
 done
