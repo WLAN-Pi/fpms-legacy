7dc327b60206a890c0f97030cc74f09b47286a5e
Nigel Bowden <wifinigel@gmail.com>
2020-08-29 18:00:02 +0100

    Added strip() to tidy up speedtest result

--- BakeBit/Software/Python/modules/utils.py
+++ BakeBit/Software/Python/modules/utils.py
@@ -36,7 +36,7 @@ class Utils(object):
             speedtest_cmd = "speedtest | egrep -w \"Testing from|Download|Upload\" | sed -r 's/Testing from.*?\(/My IP: /g; s/\)\.\.\.//g; s/Download/D/g; s/Upload/U/g; s/bit\/s/bps/g'"
 
             try:
-                speedtest_output = subprocess.check_output(speedtest_cmd, shell=True).decode()
+                speedtest_output = subprocess.check_output(speedtest_cmd, shell=True).decode().strip()
                 speedtest_info = speedtest_output.split('\n')
             except subprocess.CalledProcessError as exc:
                 output = exc.output.decode()
