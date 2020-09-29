8d8ca5e1586c872020c599715b7e5f7ce36f3872
Jiri Brejcha <jirka@jiribrejcha.net>
2020-09-29 12:27:50 +0000

    Improved message content, now sends uplink interface details too

--- BakeBit/Software/Python/scripts/networkinfo/telegrambot.sh
+++ BakeBit/Software/Python/scripts/networkinfo/telegrambot.sh
@@ -41,8 +41,8 @@ fi
 logger "networkinfo telegrambot: Collecting data"
 
 #Collect all data
-ETH0SPEED=$(ethtool eth0 2>/dev/null | grep -q "Link detected: yes" && ethtool eth0 2>/dev/null | grep "Speed" | sed 's/....$//' | cut -d ' ' -f2  || echo "Disconnected")
-ETH0DUPLEX=$(ethtool eth0 2>/dev/null | grep -q "Link detected: yes" && ethtool eth0 2>/dev/null | grep "Duplex" | cut -d ' ' -f 2 || echo "Disconnected")
+ETH0SPEED=$(ethtool eth0 2>/dev/null | grep -q "Link detected: yes" && ethtool eth0 2>/dev/null | grep "Speed" | sed 's/....$//' | cut -d ' ' -f2  || echo "disconnected")
+ETH0DUPLEX=$(ethtool eth0 2>/dev/null | grep -q "Link detected: yes" && ethtool eth0 2>/dev/null | grep "Duplex" | cut -d ' ' -f 2 || echo "disconnected")
 HOSTNAME=$(hostname)
 UPTIME=$(uptime -p | cut -c4-)
 MODE=$(cat /etc/wlanpi-state)
@@ -54,47 +54,65 @@ PUBLICIPCOUNTRY=$(echo "$DATAINJSON" | grep -Po '"country":"\K[^"]*')
 PUBLICIPASNORG=$(echo "$DATAINJSON" | grep -Po '"asn_org":"\K[^"]*')
 PUBLICIPHOSTNAME=$(echo "$DATAINJSON" | grep -Po '"hostname":"\K[^"]*')
 PUBLICIPASN=$(echo "$DATAINJSON" | grep -Po '"asn":"\K[^"]*')
+ETH0IP=$(ip a | grep "eth0" | grep "inet" | grep -v "secondary" | head -n1 | cut -d '/' -f1 | cut -d ' ' -f6)
+UPLINK=$(ip route show | grep "default" | cut -d " " -f5)
+UPLINKIP=$(ip a | grep "$UPLINK" | grep "inet" | grep -v "secondary" | head -n1 | cut -d '/' -f1 | cut -d ' ' -f6)
 
-while true; do
-  ETH0IP=$(ip a | grep "eth0" | grep "inet" | grep -v "secondary" | head -n1 | cut -d '/' -f1 | cut -d ' ' -f6)
+if [ -z "$ETH0IP" ]; then
+  CURRENTIP="$UPLINKIP"
+else
+  CURRENTIP="$ETH0IP"
+fi
 
-  if [ ! -z "$ETH0IP" ]; then
-    TEXT=''
-    TEXT+="%f0%9f%9f%a2 <b>$HOSTNAME is now online</b> %0A"
-    TEXT+="Eth0 IP address: <code>$ETH0IP</code> %0A"
-    TEXT+="Speed: $ETH0SPEED %0A"
-    TEXT+="Duplex: $ETH0DUPLEX %0A"
-    TEXT+="Mode: $MODE %0A"
-    TEXT+="Uptime: $UPTIME %0A"
-    TEXT+="Web interface: http://$ETH0IP %0A"
-    #TEXT+="Web console: https://$ETH0IP:9090 %0A"
-    TEXT+="SSH: <code>ssh://wlanpi@$ETH0IP</code> %0A"
-    #TEXT+="Copy file to TFTP server: copy flash:filename tftp://$ETH0IP %0A"
-    TEXT+="Public IP: <code>$PUBLICIP</code>, <code>$PUBLICIPHOSTNAME</code> %0A"
+#Compose the message
+TEXT=''
+TEXT+="%f0%9f%9f%a2 <b>$HOSTNAME is now online</b> %0A"
+if [ "$ETH0IP" ]; then
+  TEXT+="Eth0 IP address: <code>$ETH0IP</code> %0A"
+fi
+if [[ "$ETH0SPEED" == "disconnected" ]]; then
+  TEXT+="Eth0 is down %0A"
+else
+  TEXT+="Eth0 speed: $ETH0SPEED %0A"
+  TEXT+="Eth0 duplex: $ETH0DUPLEX %0A"
+fi
+TEXT+="WLAN Pi mode: $MODE %0A"
+TEXT+="Uptime: $UPTIME %0A"
 
-    #Try using this instead for complex text
-    #curl --data chat_id=12345678 --data-urlencode "text=Some complex text $25 78%"  "https://api.telegram.org/bot0000000:KEYKEYKEYKEYKEYKEY/sendMessage"
+TEXT+="%0A"
+TEXT+="Uplink to internet: $UPLINK %0A"
+if [[ "$UPLINK" != "eth0" ]]; then
+  TEXT+="Local $UPLINK IP address: $UPLINKIP %0A"
+fi
+TEXT+="Public IP: <code>$PUBLICIP</code>, <code>$PUBLICIPHOSTNAME</code> %0A"
 
-    #First attempt to send message
-    timeout 5 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_API_KEY/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=html&text=$TEXT" > /dev/null
-    if [ "$?" != 0 ]; then
-      echo "Message failed! Resending now."
-      #Second attempt to send message
-      timeout 5 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_API_KEY/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=html&text=$TEXT" > /dev/null
+if [ ! -z "$CURRENTIP" ]; then
+  TEXT+="%0A"
+  TEXT+="Web interface: http://$CURRENTIP %0A"
+  #TEXT+="Web console: https://$CURRENTIP:9090 %0A"
+  TEXT+="SSH: <code>ssh://wlanpi@$CURRENTIP</code> %0A"
+  #TEXT+="Copy file to TFTP server: copy flash:filename tftp://$CURRENTIP %0A"
+fi
 
-      if [ "$?" != 0 ]; then
-        echo "Sending failed again! Giving up."
-        exit 3
-      else
-        echo "Message successfully sent at second attempt"
-      fi
-    else
-      echo "Message successfully sent"
-      logger "networkinfo telegrambot: Message successfully sent"
-    fi
-    break
+#Try using this instead for complex text
+#curl --data chat_id=12345678 --data-urlencode "text=Some complex text $25 78%"  "https://api.telegram.org/bot0000000:KEYKEYKEYKEYKEYKEY/sendMessage"
+
+#First attempt to send message
+timeout 5 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_API_KEY/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=html&text=$TEXT" > /dev/null
+if [ "$?" != 0 ]; then
+  sleep 3
+  echo "Error: First attempt to send message failed! Resending now..."
+  #Second attempt to send message
+  timeout 5 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_API_KEY/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=html&text=$TEXT" > /dev/null
+
+  if [ "$?" != 0 ]; then
+    echo "Error: Second attempt to send meesage failed! Giving up."
+    exit 3
+  else
+    echo "Message successfully sent at second attempt"
   fi
-  sleep 1
-  echo "Waiting for internet connectivity..."
-done
+else
+  echo "Message successfully sent"
+  logger "networkinfo telegrambot: Message successfully sent"
+fi
 
