171101bb781600c7831fd66599411741a35130c6
Jiri Brejcha <jirka@jiribrejcha.net>
2020-09-29 10:57:33 +0000

    Cleanup in comments

--- BakeBit/Software/Python/scripts/networkinfo/networkinfo.sh
+++ BakeBit/Software/Python/scripts/networkinfo/networkinfo.sh
@@ -6,7 +6,7 @@ DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #Start neighbour detection immediately after the WLAN Pi boots up
 sudo "$DIRECTORY"/lldpneigh.sh &
 sudo "$DIRECTORY"/cdpneigh.sh  &
-#timeout 10 "$DIRECTORY"/telegrambot.sh &
+#Start monitoring internet connectivity immediately after the WLAN Pi boots up
 "$DIRECTORY"/watchinternet.sh &
 
 #Monitor up/down status changes of eth0 and execute neighbour detection or cleanup
@@ -19,7 +19,6 @@ do
     #Execute neighbour detection scripts
     sudo "$DIRECTORY"/lldpneigh.sh &
     sudo "$DIRECTORY"/cdpneigh.sh  &
-    #timeout 10 "$DIRECTORY"/telegrambot.sh &
   ;;
   *".ethernet eth0: Link is Down"*)
     logger "networkinfo script: eth0 went down"
