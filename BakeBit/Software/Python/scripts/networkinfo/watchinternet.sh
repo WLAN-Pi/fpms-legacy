88ed3c4e438a5340e1b8176e7a67da2d47ff91c8
Jiri Brejcha <jirka@jiribrejcha.net>
2020-10-02 14:05:19 +0000

    Watch internet output is now not as verbose

--- BakeBit/Software/Python/scripts/networkinfo/watchinternet.sh
+++ BakeBit/Software/Python/scripts/networkinfo/watchinternet.sh
@@ -10,41 +10,37 @@ DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 logger "networkinfo watchinternet: Started"
 
 function gone_online {
-echo -e "\e[92mChanged to online\033[0m"
+echo -e "\e[92mWLAN Pi is now online\033[0m"
 logger "networkinfo watchinternet: WLAN Pi is now online"
+timeout 10 "$DIRECTORY"/telegrambot.sh >/dev/null 2>&1 &
 }
 
 function gone_offline {
-echo -e "\e[91mChanged to offline\033[0m"
+echo -e "\e[91mWLAN Pi is now offline\033[0m"
 logger "networkinfo watchinternet: WLAN Pi is now offline"
 }
-
-echo "-----------------------"
 
 while true; do
 
   #Current default route interface
-  DEFAULT_ROUTE_VIA=$(ip route show | grep "default" | cut -d " " -f5)
-  if [ -z "$DEFAULT_ROUTE_VIA" ]; then
-    echo -e "\e[91mNo default route\033[0m"
-  else
-    echo "Default route via: $DEFAULT_ROUTE_VIA"
+  DEFAULT_ROUTE_CURRENTLY=$(ip route show | grep "default" | cut -d " " -f5)
+  #if [ -z "$DEFAULT_ROUTE_CURRENTLY" ]; then
+  #  echo -e "\e[91mNo default route\033[0m"
+  #fi
+
+  if [[ "$DEFAULT_ROUTE_CURRENTLY" != "$DEFAULT_ROUTE_PREVIOUSLY" ]] && [[ "$DEFAULT_ROUTE_CURRENTLY" ]]; then
+    echo "Default route via: $DEFAULT_ROUTE_CURRENTLY"
   fi
 
   if timeout 3 nc -zw1 canireachthe.net 443 2>/dev/null 1>/dev/null; then
     CURRENTLY="online"
-    echo "Currently: $CURRENTLY"
-    echo "Previously: $PREVIOUSLY"
   else
     CURRENTLY="offline"
-    echo "Currently: $CURRENTLY"
-    echo "Previously: $PREVIOUSLY"
   fi
 
   #WLAN Pi is now online
   if [[ "$CURRENTLY" == "online" ]] && [[ "$PREVIOUSLY" == "offline" ]]; then
     gone_online
-    timeout 10 "$DIRECTORY"/telegrambot.sh >/dev/null 2>&1 &
   fi
 
   #WLAN Pi is now offline
@@ -53,7 +49,9 @@ while true; do
   fi
 
   PREVIOUSLY="$CURRENTLY"
-  echo "-----------------------"
   sleep 3
+
+  DEFAULT_ROUTE_PREVIOUSLY="$DEFAULT_ROUTE_CURRENTLY"
+
 done
 
